ba37e72f52a965172a1bdb08ab5617ea51e24db6
Bruno Santos <bruno403f@gmail.com>
2017-06-30 17:11:30 -0400

    Adição da coleção de disciplinas, modificação da coleção de estrutura curricular (recebe o _id da disciplina como "chave estrangeira") e correções de integridade do banco de dados.

--- server/uploadcurricularstructure.js
+++ server/uploadcurricularstructure.js
@@ -1,42 +1,85 @@
 import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 
+
+// Coleção contendo a estrutura curricular de cada disciplina
 CurricularStructure = new Meteor.Collection('curricularstructure');
 
-/* Formato dos dados
-{
-  codigo: 90000008,
-  nome: CÁLCULO II,
-  semestre: 2,
-  creditos: 4,
-  prereq1: CÁLCULO I,
-  prereq2: null,
-  prereq3: null,
-}*/
+// Coleção contendo as disciplinas registradas
+Disciplines = new Meteor.Collection('disciplines');
+
+
+/* Formato dos dados das coleções (exemplo):
+
+ Disciplines:
+ {
+   _id: HYXZ
+   codigo: 90000007,
+   nome: CÁLCULO I,
+   creditos: 4
+ }
+ {
+   _id: XYZ8
+   codigo: 90000008,
+   nome: CÁLCULO II,
+   creditos: 4
+ }
+
+ CurricularStructure:
+ {
+   _id: ...
+   idDisciplina: HYXZ
+   semestre: 1,
+   prereq: [ ]
+ }
+ {
+   _id: ...
+   idDisciplina: XYZ8
+   semestre: 2,
+   prereq: [ HYXZ ]
+ }
+ */
 
 Meteor.methods({
+
   uploadCurricularStruture( data ) {
     check( data, Array );
     const currentUser = Meteor.userId();
 
     data.forEach(item => {
-      exists = CurricularStructure.find({
-                 disciplina: item.disciplina,
-                 semestre: item.semestre,
-                 createdBy: currentUser
+      const existCurr = CurricularStructure.find({
+        nome: item.nome,
+        createdBy: currentUser
+      }).count();
+      const existDisc = Disciplines.find({
+        nome: item.nome
       }).count();
 
-      if ( !exists ) {
-        CurricularStructure.insert({
-                 codigo: item.codigo,
-                 nome: item.nome,
-                 semestre: item.semestre,
-                 creditos: item.creditos,
-                 prereq1: item.prereq1,
-                 prereq2: item.prereq2,
-                 prereq3: item.prereq3,
-                 createdBy: currentUser
+      if ( existCurr === 0 && existDisc === 0) {
+
+        const idDisciplina = Disciplines.insert({
+          codigo: item.codigo,
+          nome: item.nome,
+          creditos: parseInt(item.creditos)
         });
+        // TODO retirar esta linha após testes concluídos.
+        console.log("Added discipline: " + idDisciplina);
+
+        const prereq1 = Disciplines.findOne({ nome: item.prereq1 });
+        const prereq2 = Disciplines.findOne({ nome: item.prereq2 });
+        const prereq3 = Disciplines.findOne({ nome: item.prereq3 });
+
+        const idCurrStr = CurricularStructure.insert({
+          idDisciplina: idDisciplina,
+          semestre: parseInt(item.semestre),
+          prereq1: (prereq1) ? prereq1._id : null,
+          prereq2: (prereq2) ? prereq1._id : null,
+          prereq3: (prereq3) ? prereq1._id : null,
+          createdBy: currentUser
+        });
+        // TODO retirar esta linha após testes concluídos.
+        console.log("Added structure: " + idCurrStr);
+
       } else {
         console.warn('Rejected. This item already exists.');
       }
