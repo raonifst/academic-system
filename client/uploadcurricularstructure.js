255fc1dd2e01a6235f78d504b72a4ddd514ae50f
Bruno Santos <bruno403f@gmail.com>
2017-07-01 13:07:13 -0400

    Correção de bug

--- client/uploadcurricularstructure.js
+++ client/uploadcurricularstructure.js
@@ -1,6 +1,5 @@
 import {Template} from 'meteor/templating';
 import {ReactiveVar} from 'meteor/reactive-var';
-import {Papa} from 'meteor/harrison:papa-parse';
 
 import './uploadcurricularstructure.html'
 
