22756f58ec69d69660d4080469d1d1663f4da6db
Bruno Santos <bruno403f@gmail.com>
2017-07-01 12:57:08 -0400

    Adição do atributo skipEmptyLines no Papa.parse da estrutura curricular

--- client/uploadcurricularstructure.js
+++ client/uploadcurricularstructure.js
@@ -1,5 +1,6 @@
 import {Template} from 'meteor/templating';
 import {ReactiveVar} from 'meteor/reactive-var';
+import {Papa} from 'meteor/harrison:papa-parse';
 
 import './uploadcurricularstructure.html'
 
@@ -8,19 +9,25 @@ Template.uploadcurricularstructure.onCreated(() => {
   Template.instance().uploading = new ReactiveVar( false );
 });
 
+
 Template.uploadcurricularstructure.helpers({
   uploading() {
     return Template.instance().uploading.get();
   }
 });
 
+
 Template.uploadcurricularstructure.events({
+
   'change [name="uploadCSV"]': function ( event, template ) {
+
     template.uploading.set( true );
 
     Papa.parse( event.target.files[0], {
       header: true,
+      skipEmptyLines: true,
       complete( results, file ) {
+
         Meteor.call( 'uploadCurricularStruture', results.data, ( error, response ) => {
           if ( error ) {
             console.log( error.reason );
@@ -29,7 +36,10 @@ Template.uploadcurricularstructure.events({
             Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
           }
         });
+
       }
     });
+
   }
+
 });
