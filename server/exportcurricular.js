4ee3aba9213fef6b254126ee25233209f2ba8853
Duarte <william.2025@hotmail.com>
2017-07-05 10:19:30 -0400

    CORREÇÃO EXPORTAR ESTRUTURA CURRICULAR

--- server/exportcurricular.js
+++ server/exportcurricular.js
@@ -10,24 +10,33 @@ Meteor.methods({
 		];
 
 		var data = [];
-		var aux = [];
+		var sem = [];
+		var pre = [];
 		var i=0;
 		var curricular = CurricularStructure.find().fetch();
 		var disciplina = Disciplines.find().fetch();
-		/*_.each(curricular, function(h) {
-			aux.push([
-				h.semestre,
-				h.prereq
+		_.each(curricular, function(h) {
+			sem.push([
+				h.semestre
 			]);
-		})*/
+		})
+
+		_.each(curricular, function(l) {
+			pre.push([
+				l.prereq
+			]);
+		})
+
+
 		_.each(disciplina, function(p) {
 			data.push([
 				p.codigo,
 				p.nome,
-				p.creditos//,
-				//aux[i]
+				p.creditos,
+				sem[i],
+				pre[i]
 			]);
-			//i=i+1;
+			i=i+1;
 		})
 		return {fields: fields, data: data};
 	}
