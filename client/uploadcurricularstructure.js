45cf94f5da79ec3f4c6b9afe2c9755bd7e757f1b
Bruno Santos <bruno403f@gmail.com>
2017-07-06 23:53:25 -0400

    Melhorias no algoritmo de verificação de erros no csv da estrutura curricular.

--- client/uploadcurricularstructure.js
+++ client/uploadcurricularstructure.js
@@ -1,6 +1,5 @@
 import {Template} from 'meteor/templating';
 import {ReactiveVar} from 'meteor/reactive-var';
-import {check} from 'meteor/check';
 
 import './uploadcurricularstructure.html'
 
@@ -16,11 +15,11 @@ Template.uploadcurricularstructure.helpers({
     return Template.instance().uploading.get();
   },
   'listaDisiciplinas': function(){
-          var currentUserId = Meteor.userId();
-          return Disciplines.find();
-          //console.log(D)
-          //return Disciplines.find(({ createdBy: currentUserId },
-            //                      { sort: {codigo: 1, nome: 1} }));
+    var currentUserId = Meteor.userId();
+    return Disciplines.find();
+    //console.log(D)
+    //return Disciplines.find(({ createdBy: currentUserId },
+    //                      { sort: {codigo: 1, nome: 1} }));
   }
 });
 
@@ -29,70 +28,35 @@ Template.uploadcurricularstructure.events({
 
   'change [name="uploadCSV"]': function ( event, template ) {
 
-    template.uploading.set( true );
+    var data = [];
+    var globalError = false;
+    template.uploading.set(true);
 
     Papa.parse( event.target.files[0], {
       header: true,
       skipEmptyLines: true,
       dynamicTyping: true,
-      error(err, file, inputElem, reason) {
-        template.uploading.set( false );
-        Bert.alert('This is not a csv valid file.', 'danger', 'growl-top-right' );
-      },
-      complete( results, file ) {
-
-        var data = results.data;
-        var schema = new SimpleSchema({
-          codigo: {
-            type: Number,
-            min: 90000001,
-            max: 99999999,
-            label: "Código da disciplina"},
-          nome: {
-            type: String,
-            label: "Nome da disciplina"
-          },
-          creditos: {
-            type: Number,
-            min: 2,
-            max: 6,
-            label: "Créditos da disciplina"
-          },
-          semestre: {
-            type: Number,
-            min: 1,
-            max: 10,
-            label: "Semestre da disciplina"
-          },
-          prereq: {
-            type: String,
-            label: "Pré-requisitos da disciplina"
-          }
-        });
-
+      step(row, parser) {
         try {
-          check(data, Array);
-          data.forEach(item => {
-            schema.validate(item);
-          });
-
+          schemaCurrDisc.validate(row.data[0]);
         } catch (err) {
-          template.uploading.set( false );
-          Bert.alert('This file is not a valid csv file: ' + err.reason,
-            'danger', 'growl-top-right' );
-          return;
+          Bert.alert('This file is not a valid csv file.', 'danger', 'growl-top-right' );
+          globalError = true;
+          template.uploading.set(false);
+          parser.abort();
         }
-
-        Meteor.call('uploadCurricularStruture', data, (error, response) => {
-          if (error) {
-            console.log(error.reason);
-            Bert.alert( 'Unknown internal error.', 'danger', 'growl-top-right' );
-          } else {
-            template.uploading.set( false );
-            Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
-          }
+        data.push(row.data[0]);
+      },
+      complete() {
+        if (globalError)
+          return;
+        Meteor.call('uploadCurricularStruture', data, (error) => {
+          if (error)
+            Bert.alert('Unknown internal error.', 'danger', 'growl-top-right');
+          else
+            Bert.alert('Upload complete.', 'success', 'growl-top-right');
+          template.uploading.set(false);
         });
-
       }
     });
 
--- common.js
+++ common.js
@@ -8,3 +8,31 @@ CurricularStructure = new Meteor.Collection('curricularstructure');
 
 // Coleção contendo as disciplinas registradas
 Disciplines = new Meteor.Collection('disciplines');
+
+schemaCurrDisc = new SimpleSchema({
+  codigo: {
+    type: Number,
+    min: 90000001,
+    max: 99999999,
+    label: "Código da disciplina"},
+  nome: {
+    type: String,
+    label: "Nome da disciplina"
+  },
+  creditos: {
+    type: Number,
+    min: 2,
+    max: 6,
+    label: "Créditos da disciplina"
+  },
+  semestre: {
+    type: Number,
+    min: 1,
+    max: 10,
+    label: "Semestre da disciplina"
+  },
+  prereq: {
+    type: String,
+    label: "Pré-requisitos da disciplina"
+  }
+});
