519200cff736a0a50c698344e01981879979ef88
jonloureiro <jonathan.loureiro.m@gmail.com>
2017-07-07 02:51:42 -0400

    Corrigindo problema de autorização no método que remove as coleções.

--- server/buttonremove.js
+++ server/buttonremove.js
@@ -1,9 +1,21 @@
+import {Meteor} from 'meteor/meteor';
+import {check} from 'meteor/check';
+
 Meteor.methods({
   clearStructure(){
+    const currentUser = Meteor.userId();
+    if (!currentUser) {
+      throw new Meteor.Error("not-logged-in", "You're not logged-in.");
+    }
     CurricularStructure.remove({});
     Disciplines.remove({});
   },
+
   clearRecords(){
+    const currentUser = Meteor.userId();
+    if (!currentUser) {
+      throw new Meteor.Error("not-logged-in", "You're not logged-in.");
+    }
     Records.remove({});
   }
 })
