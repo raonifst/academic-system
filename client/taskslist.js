113f3e6db878a12f71e99907ce4c71e61dc63f3a
Raoni Teixeira <raoniteixeira@gmail.com>
2017-07-10 21:57:15 -0400

    improving the font size at configuration card

--- client/taskslist.js
+++ client/taskslist.js
@@ -7,29 +7,43 @@ Meteor.subscribe('userStats');
 
 
 Template.taskslist.helpers({
-
-  checkTask1: function () {
+  task: function() {
     const currentUserId = Meteor.userId();
-    if (currentUserId) {
-      if (Users.findOne({ idUser: currentUserId }).changedDefaultPassword)
-        return "checked";
+    const user = Users.findOne({ idUser: currentUserId });
+    let tasks = [{}];
+
+    if(currentUserId && user) {
+
+      tasks = [
+        {
+            number: '1',
+            name: "Alterar senha padrão",
+            link: "changepass",
+            status:  Users.findOne({ idUser: currentUserId }).changedDefaultPassword
+        },
+        {
+          number: '2',
+          name:'Fazer upload da matriz (estrutura) curricular de disciplinas',
+          link: 'uploadcurricularstructure',
+          status: Users.findOne({ idUser: currentUserId }).uploadedCurricularStructure
+        },
+        {
+          number: '3',
+          name: ' Fazer upload do histórico acadêmico dos alunos',
+          link: 'uploadacademicrecord',
+          status: Users.findOne({ idUser: currentUserId }).uploadedAcademicRecords
+        }
+      ];
     }
+    return tasks;
   },
 
-  checkTask2: function () {
+  done: function () {
     const currentUserId = Meteor.userId();
-    if (currentUserId) {
-      if (Users.findOne({ idUser: currentUserId }).uploadedCurricularStructure)
-        return "checked";
-    }
-  },
-
-  checkTask3: function () {
-    const currentUserId = Meteor.userId();
-    if (currentUserId) {
-      if (Users.findOne({ idUser: currentUserId }).uploadedAcademicRecords)
-        return "checked";
-    }
+    const user = Users.findOne({ idUser: currentUserId });
+    return user && user.changedDefaultPassword &&
+      user.uploadedCurricularStructure && user.uploadedAcademicRecords;
   }
+
 
 });
