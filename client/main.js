0007b72be323bdcc08db0ee7dae19867d8f9c4d4
Raoni Florentino da Silva Teixeira <raonifst@Mac-mini-de-Raoni.local>
2017-07-12 22:56:20 -0400

    fixing some bugs related with login/changing of users password

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import {Template} from 'meteor/templating';
+import { Accounts } from 'meteor/accounts-base'
 
 import './uploadacademicrecord.html';
 import './main.html';
@@ -93,16 +94,7 @@ Template.login.helpers({
 Template.login.events({
   'submit form': function (event) {
     event.preventDefault();
-  },
-  'change': function(event) {
-    Template.instance().validEmail.set($('#email').valid());
-    Template.instance().validPassword.set($('#password').valid());
-  },
-  'onkeydown': function(event) {
-    Template.instance().validEmail.set($('#email').valid());
-    Template.instance().validPassword.set($('#password').valid());
   }
-
 });
 
 Template.changepass.events({
@@ -138,9 +130,14 @@ $.validator.setDefaults({
 Template.login.onRendered(function () {
 
   var validator = $('.login').validate({
+
+    onkeyup: false,
+    keypress: false,
+
     errorPlacement: function (error, element) {
             Bert.alert(error.text(),'danger' );
     },
+
     submitHandler: function (event) {
       var email = $('[name=email]').val();
       var password = $('[name=password]').val();
@@ -148,15 +145,11 @@ Template.login.onRendered(function () {
         if (error) {
           if (error.reason === "User not found") {
             Bert.alert( 'Usuário não cadastrado', 'danger' );
-            /*validator.showErrors({
-              email: 'Usuário não cadastrado.'
-            });*/
+
           }
           if (error.reason === "Incorrect password") {
             Bert.alert( 'Senha incorreta', 'danger' );
-            /*validator.showErrors({
-              password: 'Senha incorreta.'
-            });*/
+
           }
         } else {
           Meteor.call('isFirstLogin', (error, results) => {
@@ -180,13 +173,29 @@ Template.changepass.onRendered( function(){
 
   var validator = $('.login').validate({
 
+    onkeyup: false,
+    keypress: false,
+
+    errorPlacement: function (error, element) {
+            Bert.alert(error.text(),'danger' );
+    },
+
     submitHandler:function(event){
       const newPassword = $('[name=password]').val();
-      Meteor.call('changeUserPassword',newPassword,function(error){
+      const oldPassword = $('[name=oldpassword]').val();
+      Accounts.changePassword(oldPassword, newPassword, function(error){
         if (error) {
-          validator.showErrors({
+          if (error.reason === "User not found") {
+            Bert.alert( 'Usuário não cadastrado', 'danger' );
+
+          }
+          if (error.reason === "Incorrect password") {
+            Bert.alert( 'Senha incorreta', 'danger' );
+
+          }
+          /*validator.showErrors({
             password:error.reason
-          })
+          })*/
         } else {
           Bert.alert('Senha alterada!', 'success', 'growl-top-right');
           Meteor.call('changeFirstLogin');
