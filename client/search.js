77e236f7185f7b5e2099e328df0c82439beac0d4
Duarte <william.2025@hotmail.com>
2017-07-28 23:26:12 -0400

    Exchange of code queries Exchange Definições de exchange substantivo an act of giving one thing and receiving another (especially of the same type or value) in return. negotiations should eventually lead to an exchange of land for peace sinônimos: interchange, trade, trading, swapping, traffic, trafficking verbo give something and receive something of the same kind in return. we exchanged addresses sinônimos: trade, swap, switch, change, interchange Veja também exchange Traduções de exchange verbo trocar exchange, change, swap, switch, interchange, barter permutar exchange, swap, barter, permute, interchange, truck comutar commute, exchange, remit cambiar change, exchange, convert, gear mudar change, switch, move, turn, alter, vary substantivo a troca exchange, swap, shift, interchange, permutation, swop o câmbio exchange, exchange rate, currency exchange, change, money exchange, conversion o intercâmbio exchange, interchange, trade, communication, reciprocity, dealing a permuta exchange, interchange, permutation, give-and-take a bolsa bag, purse, handbag, pouch, exchange, pocket

--- client/search.js
+++ client/search.js
@@ -1,60 +1,131 @@
+Template.search.onCreated(() => {
+  Template.instance().courseName = new ReactiveVar('');
+  Template.instance().isStudent = new ReactiveVar(true);
+  Template.instance().countStudentsWhoMustEnrollInACourse = new ReactiveVar(0);
+  Template.instance().countStudentsWhoHavePrerequisitesForACourse = new ReactiveVar(0);
+});
 Template.search.onCreated( () => {
   let template = Template.instance();
 
   template.searchQuery = new ReactiveVar();
-  template.searching   = new ReactiveVar( false );
-
-  template.autorun( () => {
-    template.subscribe( 'studentsnocoursed', template.searchQuery.get(), () => {
-      setTimeout( () => {
-        template.searching.set( false );
-      }, 300 );
-    });
+});
+function hash2array(map) {
+  let array = [];
+  Object.keys(map).forEach(function(key){
+    array.push(map[key]);
   });
-});
-Template.search.helpers({
-  studentsnocoursed() {
-    let courseName = Template.instance().searchQuery.get();
-    let b = [];
-    console.log(courseName);
-    let studentsnocoursed = [];
-    Records.find().forEach(
-      function(p){b.push(p.nome)
-      });
-    if (courseName) {
-    let regex = new RegExp(courseName, 'i');
-    Records.find({disciplina: regex, situacao: "AP"}, {_id:0}).forEach(
-      function(rec){studentsnocoursed.push(rec.rga)
-      });
-      let a = [];
-      console.log(studentsnocoursed);
-      Records.find({'rga':{'$nin':studentsnocoursed}},{sort:{rga:1}}).forEach(
-        function(rec){a.push(rec.nome)
-        });
-        var distinctArray = _.uniq(a)
-        //console.log(distinctArray);
-        return distinctArray;
-      }
-      else{
-        var distinctArray = _.uniq(b)
-        return distinctArray;
-      }
-      /*_.each(teste, function(h) {
-        a.push([h.nome]);
-      });*/
-      //console.log(a);
+  return array;
+}
+/*Funcao auxiliar para queries*/
+function auxStudentsWhoMustEnrollInACourse(courseName){
 
+    let studentsApprovedInCourse = [];
+    Records.find({disciplina: courseName, situacao: "AP"}, {_id: 0}).forEach(
+                  function(rec){studentsApprovedInCourse.push(rec.rga)
+                });
+
+    var map = {};
+
+    Records.find( { 'rga': {'$nin': studentsApprovedInCourse}}, { sort: { rga: 1 } } ).forEach(
+                   function(rec){
+                     if(!map[rec.rga]) {
+                        map[rec.rga] = {
+                           nome: rec.nome,
+                           rga: rec.rga
+                       }
+                     }
+                   });
+
+    return hash2array(map);
+}
+
+Template.search.helpers({
+  isStudent: function() {
+    return Template.instance().isStudent.get();
+  },
+
+  settings: function () {
+      return {
+          rowsPerPage: 10,
+          showFilter: true,
+          fields: [
+            { key: 'rga', label: 'RGA' , cellClass: 'col-md-4'},
+            { key: 'nome', label: 'Nome' , cellClass: 'col-md-4'}
+          ]
+      };
+  },
+
+  countStudentsWhoMustEnrollInACourse: function() {
+    Template.search.__helpers.get('studentsWhoMustEnrollInACourse').call();
+    return Template.instance().countStudentsWhoMustEnrollInACourse.get();
+  },
+  countStudentsWhoHavePrerequisitesForACourse: function() {
+    Template.search.__helpers.get('studentsWhoHavePrerequisitesForACourse').call();
+    return Template.instance().countStudentsWhoHavePrerequisitesForACourse.get();
+  },
+
+
+  studentsWhoHavePrerequisitesForACourse: function(){
+
+    let courseName = Template.instance().courseName.get();
+    //console.log(courseName)
+    if(courseName != '') {
+      let candidates = auxStudentsWhoMustEnrollInACourse(courseName);
+      //console.log(courseName)
+      courseId = Disciplines.findOne({nome: courseName}, {fields:{_id:1}});
+      courseId = courseId==null?'':courseId._id;
+      //console.log(courseId);
+      let prereq = CurricularStructure.findOne({idDisciplina: courseId}, {fields: {prereq: 1}});
+      prereq = prereq == null?[]:prereq.prereq;
+    var map = {};
+    //console.log(prereq)
+    candidates.forEach(function(student) {
+      let count = 0;
+      prereq.forEach(function(code) {
+
+        courseName = Disciplines.findOne({_id: code}, {fields: {nome: 1}}).nome;
+        let exist = Records.findOne({rga: student.rga, disciplina: courseName, situacao: "AP"}, {_id: 0});
+        if(exist)
+          count = count + 1;
+      });
+
+      if(count == prereq.length) {
+
+        map[student.rga] = {
+           nome: student.nome,
+           rga: student.rga
+       }
+      }
+    });
+      let result = hash2array(map);
+      Template.instance().countStudentsWhoHavePrerequisitesForACourse.set(result.length);
+      return result;
+    }
+    else { Template.instance().countStudentsWhoHavePrerequisitesForACourse.set(0);return [{}]};
+  },
+
+  studentsWhoMustEnrollInACourse: function(){
+      let courseName = Template.instance().courseName.get();
+      if(courseName == '') {
+        Template.instance().countStudentsWhoMustEnrollInACourse.set(0);
+        return [{}];
+      }
+      let result = auxStudentsWhoMustEnrollInACourse(courseName);
+      Template.instance().countStudentsWhoMustEnrollInACourse.set(result.length);
+      return result;
   }
-});
+
+
+})
 Template.search.events({
   'keyup [name="search"]' ( event, template ) {
     let value = event.target.value.trim();
     if ( value !== '' && event.keyCode === 13 ) {
-      template.searchQuery.set( value );
+      Template.instance().courseName.set( value );
     }
 
     if ( value === '' ) {
-      template.searchQuery.set( value );
+      Template.instance().courseName.set( value );
     }
   }
 });
