36c430b2999f95d4cb596f7ade77126fc32e2f8d
jonloureiro <jonathan.loureiro.m@gmail.com>
2017-08-12 15:55:36 -0400

    adding form

--- client/scripts/dataeditadd.js
+++ client/scripts/dataeditadd.js
@@ -1,6 +1,9 @@
 import Courses from "../../imports/api/collections/courses";
 import Records from "../../imports/api/collections/records"
 
+Template.add.helpers({
+});
+
 Template.modaladd.helpers({
   coursesOrRecords(collection) {
     if (collection == 'Disciplina')
@@ -9,9 +12,33 @@ Template.modaladd.helpers({
   }
 });
 
-Template.addcourse.events({
+Template.add.events({
   'submit form': function(event) {
     event.preventDefault();
-    console.log(event.target.codigo.value);
+    if (this.collection == 'Disciplina') {
+      console.log(event.target.codigo.value);
+      console.log(event.target.nome.value);
+      console.log(event.target.creditos.value);
+      console.log(event.target.semestre.value);
+      console.log(event.target.prereq.value);
+      event.target.nome.value = '';
+      event.target.creditos.value = '';
+      event.target.semestre.value = '';
+      event.target.prereq.value = '';
+      event.target.codigo.value = '';
+    } else {
+      console.log(event.target.rga.value);
+      console.log(event.target.nome.value);
+      console.log(event.target.disciplina.value);
+      console.log(event.target.situacao.value);
+      console.log(event.target.ano.value);
+      console.log(event.target.semestre.value);
+      event.target.rga.value = '';
+      event.target.nome.value = '';
+      event.target.disciplina.value = '';
+      event.target.situacao.value = '';
+      event.target.ano.value = '';
+      event.target.semestre.value = '';      
+    }
   }
 });
