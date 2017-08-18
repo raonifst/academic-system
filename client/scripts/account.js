c8b7f50dfd16baa8b2b8970fddfa6898e10a76cf
jonloureiro <jonathan.loureiro.m@gmail.com>
2017-08-17 21:56:07 -0400

    adding user information

--- client/scripts/account.js
+++ client/scripts/account.js
@@ -1,11 +1,3 @@
-Template.layout.events({
-  'click .logout': function (event) {
-    event.preventDefault();
-    Meteor.logout();
-    Router.go('login');
-  }
-});
-
 Template.login.events({
   'submit form': function(event){
     event.preventDefault();
@@ -20,5 +12,26 @@ Template.navigation.events({
     'click .exit': function(event){
         event.preventDefault();
         Meteor.logout();
+        Router.go('/');
     }
+});
+
+Template.navigation.helpers({
+  nome() {
+    return Meteor.user().name;
+  },
+  curso() {
+    return Meteor.user().gradProgram;
+  },
+
+});
+
+Template.navbar.helpers({
+  ano() {
+    return Meteor.user().currentYear;
+  },
+
+  semestre() {
+    return Meteor.user().currentSemester;
+  }
 });
