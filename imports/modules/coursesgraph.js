057bfa58d4e394306c9f37cea8358e1566387d22
Bruno Santos <bruno403f@gmail.com>
2017-08-20 18:39:20 -0400

    improvements on CourseDAG class: now topological order list and DFS results are attributes

--- imports/modules/coursesgraph.js
+++ imports/modules/coursesgraph.js
@@ -57,11 +57,18 @@ export class CVertexAttr {
 export class CoursesDAG {
   constructor(coursesArray) {
     this.gMap = new Map();
+    this.topologicalOrderList = null;
+    this.attributes = null;
     coursesArray.forEach(item => {
       this.gMap.set(item.codigo, item.prereq);
     });
-    this._validate();
+    this._validate(); // Verifica se existem códigos inválidos ou ciclos no DAG
+    this.topologicalOrderList = this.attributes = null;
     this.transpose();
+    // Para fazer uma segunda validação do grafo transposto (não é necessário) descomente as
+    // duas linhas abaixo:
+    //this._validate();
+    //this.topologicalOrderList = this.attributes = null;
   }
 
   _validate() {
@@ -88,22 +95,27 @@ export class CoursesDAG {
     this.gMap = revMap;
   }
 
-  depthFirstSearch(list) {
-    var attr = new CVertexAttr();
-    for (var key1 of this.gMap.keys()) {
-      attr.setColorTo(key1, GraphColors.WHITE);
-      attr.setPiTo(key1, null);
-      attr.setDiscoveredTimeTo(key1, -1);
-    }
-    for (var key2 of this.gMap.keys()) {
-      if (attr.getColorFrom(key2) == GraphColors.WHITE)
-        this._depthFirstSearchVisit(key2, attr, list);
-    }
-    // TODO remove this debug line later
-    console.log(attr);
+  depthFirstSearch() {
+    return this._depthFirstSearch(null);
   }
 
-  _depthFirstSearchVisit(initialVertexCode, vAttr, outputList) {
+  _depthFirstSearch(list) {
+    this.attributes = new CVertexAttr();
+    for (var key1 of this.gMap.keys()) {
+      this.attributes.setColorTo(key1, GraphColors.WHITE);
+      this.attributes.setPiTo(key1, null);
+      this.attributes.setDiscoveredTimeTo(key1, -1);
+    }
+    for (var key2 of this.gMap.keys()) {
+      if (this.attributes.getColorFrom(key2) == GraphColors.WHITE)
+        this._depthFirstSearchVisit(key2, list);
+    }
+    // TODO remove this debug line later
+    console.log(this.attributes);
+    return this.attributes;
+  }
+
+  _depthFirstSearchVisit(initialVertexCode, outputList) {
     var stack = [];
     var uKey, uAdjList, isVertexLeaf;
     this.time = 0;
@@ -112,21 +124,21 @@ export class CoursesDAG {
       isVertexLeaf = 1;
       uKey = stack.pop();
       uAdjList = this.gMap.get(uKey);
-      vAttr.setColorTo(uKey, GraphColors.GRAY);
+      this.attributes.setColorTo(uKey, GraphColors.GRAY);
       // TODO remove this debug line later
-      console.log("Vertex " + uKey + " changed color to " + vAttr.getColorFrom(uKey));
-      vAttr.setDiscoveredTimeTo(uKey, ++this.time);
+      console.log("Vertex " + uKey + " changed color to " + this.attributes.getColorFrom(uKey));
+      this.attributes.setDiscoveredTimeTo(uKey, ++this.time);
       // TODO remove this debug line later
-      console.log("Vertex " + uKey + " changed dtime to " + vAttr.getDiscoveredTimeFrom(uKey));
+      console.log("Vertex " + uKey + " changed dtime to " + this.attributes.getDiscoveredTimeFrom(uKey));
       uAdjList.forEach(vKey => {
-        var vColor = vAttr.getColorFrom(vKey);
+        var vColor = this.attributes.getColorFrom(vKey);
         var vAdjList = this.gMap.get(vKey);
         if (!vAdjList) {
           throw new Meteor.Error("graph-invalid-keys", msgCoursesGraph.msgCourseNotFound);
         } else if (vColor == GraphColors.WHITE) {
-          vAttr.setPiTo(vKey, uKey);
+          this.attributes.setPiTo(vKey, uKey);
           // TODO remove this debug line later
-          console.log("Vertex " + vKey + " changed pi to " + vAttr.getColorFrom(vKey));
+          console.log("Vertex " + vKey + " changed pi to " + this.attributes.getColorFrom(vKey));
           stack.push(vKey);
           isVertexLeaf = 0;
         } else if (vColor == GraphColors.GRAY) { // Grafo contém ciclo
@@ -134,12 +146,12 @@ export class CoursesDAG {
         }
       });
       if (isVertexLeaf) {
-        vAttr.setColorTo(uKey, GraphColors.BLACK);
+        this.attributes.setColorTo(uKey, GraphColors.BLACK);
         // TODO remove this debug line later
-        console.log("Vertex " + uKey + " changed color to " + vAttr.getColorFrom(uKey));
-        vAttr.setFinishedTimeTo(uKey, ++this.time);
+        console.log("Vertex " + uKey + " changed color to " + this.attributes.getColorFrom(uKey));
+        this.attributes.setFinishedTimeTo(uKey, ++this.time);
         // TODO remove this debug line later
-        console.log("Vertex " + uKey + " changed finished time to " + vAttr.getFinishedTimeFrom(uKey));
+        console.log("Vertex " + uKey + " changed finished time to " + this.attributes.getFinishedTimeFrom(uKey));
         if (outputList != null)
           outputList.push(uKey);
       }
@@ -147,15 +159,17 @@ export class CoursesDAG {
   }
 
   topologicalSort() {
-    var list = [];
-    try {
-      this.depthFirstSearch(list);
-    } catch (e) {
-      if (e.error == "graph-contains-cycles")
-        return [];
-      if (e.error == "graph-invalid-keys")
-        return null;
+    if (this.topologicalOrderList == null) {
+      this.topologicalOrderList = [];
+      try {
+        this._depthFirstSearch(this.topologicalOrderList);
+      } catch (e) {
+        if (e.error == "graph-contains-cycles")
+          this.topologicalOrderList = [];
+        if (e.error == "graph-invalid-keys")
+          this.topologicalOrderList = null;
+      }
     }
-    return list;
+    return this.topologicalOrderList;
   }
 }
