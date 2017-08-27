2572b2cecc666639abc8edf2222b38cbdfc2f603
Bruno Santos <bruno403f@gmail.com>
2017-08-27 06:01:33 -0400

    started implementing algorithm for suggesting disciplines to students

--- client/scripts/searchbox.js
+++ client/scripts/searchbox.js
@@ -34,8 +34,24 @@ Template.searchbox.events({
 
     //Template.instance().courseName.set(name);
     Session.set('courseName', name);
-
     if (radioValue == 'a') {
+      // TODO ao final da implementação de sugestão de disciplinas, remover este bloco de testes
+
+      // ***** Início de bloco de teste *****
+
+      console.log("Aluno selecionado: " + name);
+      console.log("Disciplinas do aluno:");
+      var recordsList = Records.find({ nome: name }).fetch();
+      console.log(recordsList);
+      var recordsDoneList = recordsList.map(function (o) { return o.disciplina; });
+      console.log(recordsDoneList);
+      console.log("Todas as disciplinas:");
+      console.log(Courses.find().fetch());
+      console.log("Disciplinas ainda não cursadas:");
+      console.log(Courses.find({ nome: { $nin: recordsDoneList } }).fetch());
+
+      // ****** Fim de bloco de teste ******
+
       Session.set('showRegister', true);
     } else if(radioValue == 'd') {
       Session.set('showRegister', false);
